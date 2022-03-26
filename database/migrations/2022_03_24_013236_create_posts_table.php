8810037373dce061aae0e977e6daa48dbfd046a2
LeonardoCosta2 <leonardo_joga10nike@hotmail.com>
2022-03-26 18:19:07 -0300

    adicionando 1 campo a migrations

--- database/migrations/2022_03_24_013236_create_posts_table.php
+++ database/migrations/2022_03_24_013236_create_posts_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->string('slug');
             $table->string('category');
             $table->string('img');
             $table->string('content');
